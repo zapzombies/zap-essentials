020828852e260bbe4c78640b0538a8beca3f7a72
thamid-gamer <60953955+thamid-gamer@users.noreply.github.com>
2021-10-09 23:03:01 -0400

    add a command to make any player visible

--- src/main/java/io/github/zap/essentials/ZAPEssentialsPlugin.java
+++ src/main/java/io/github/zap/essentials/ZAPEssentialsPlugin.java
@@ -5,10 +5,16 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Plugin for general utilities for ZAP
@@ -17,8 +23,8 @@ public class ZAPEssentialsPlugin extends ZapPlugin {
 
     @Override
     public void doEnable() {
-        this.getCommand("serializestack").setExecutor((commandSender, command, s, strings) -> {
-            if (commandSender instanceof Player player) {
+        this.getCommand("serializestack").setExecutor((sender, command, label, args) -> {
+            if (sender instanceof Player player) {
                 ItemStack itemStack = player.getInventory().getItemInMainHand();
                 FileConfiguration config = new YamlConfiguration();
                 config.set("stack", itemStack);
@@ -29,6 +35,22 @@ public class ZAPEssentialsPlugin extends ZapPlugin {
                         ),
                         Component.text(" to copy!", NamedTextColor.YELLOW)
                 ));
+            }
+
+            return true;
+        });
+        this.getCommand("lemmesee").setExecutor((sender, command, label, args) -> {
+            if (args.length < 1 || !(sender instanceof Player player)) {
+                return true;
+            }
+
+            Player show = Bukkit.getPlayer(args[0]);
+            if (show == null) {
+                return true;
+            }
+
+            for (Plugin plugin : this.getServer().getPluginManager().getPlugins()) {
+                player.showPlayer(plugin, show);
             }
 
             return true;
